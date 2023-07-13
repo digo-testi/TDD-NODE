fd21137a564c1cb0e6d9235efaf2397e2581c034
Diego <diego.testi@loviz.com.vc>
2023-07-13 20:39:18 -0300

    feat: ensure signupController return an error if n email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,17 @@
 export class SignUpController {
     handle(httprequest: any): any {
-        return { 
-            statusCode: 400,
-            body: new Error('Missing param: name')
+
+        if(!httprequest.body.name){
+            return { 
+                statusCode: 400,
+                body: new Error('Missing param: name')
+            }
+        }
+        if(!httprequest.body.name){
+            return { 
+                statusCode: 400,
+                body: new Error('Missing param: email')
+            }
         }
     }
 }
