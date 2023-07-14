4764ed597ca0a2030631d3acb4cc2fd027c2fd8f
Diego <diego.testi@loviz.com.vc>
2023-07-13 21:29:42 -0300

    test: esure signupController.handle implements correct inferfaces

--- src/presentation/controllers/signuo.spec.ts
+++ src/presentation/controllers/signuo.spec.ts
@@ -28,4 +28,18 @@ describe("SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   });
+  
+  test("Should return 400 if no password confirmation is provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email",
+        password: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: passwordConfirmation'))
+  });
 });
