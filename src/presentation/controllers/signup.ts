3f2189ed84b506b41cff34dbc469ff1fae32f625
Diego Testi <diego.testi@loviz.com.vc>
2023-07-26 15:16:52 -0300

    feat: ensure signupController calls addAccount with correct values

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,12 +1,16 @@
+import { AddAccount } from "../../domain/usecases/add-account"
 import { MissingParamError, InvalidParamError } from "../errors"
 import { badRequest, serverError } from "../helpers/http-helper"
 import { HttpRequest, HttpResponse, Controller, EmailValidator } from "../protocols"
 
 export class SignUpController implements Controller {
     private readonly emailValidator: EmailValidator
+    private readonly addAccount: AddAccount
 
-    constructor(emailValidator: EmailValidator) {
+    constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
         this.emailValidator = emailValidator
+        this.addAccount = addAccount
+
     }
 
     handle(httpRequest: HttpRequest): HttpResponse {
@@ -18,8 +22,8 @@ export class SignUpController implements Controller {
                     return badRequest(new MissingParamError(field))
                 }
             }
-            const { email, password, passwordConfirmation } = httpRequest.body
-            
+            const { name, email, password, passwordConfirmation } = httpRequest.body
+
             if (password !== passwordConfirmation){
                 return badRequest(new InvalidParamError('passwordConfirmation'))
             }
@@ -27,6 +31,11 @@ export class SignUpController implements Controller {
             if (!isValid) {
                 return badRequest(new InvalidParamError('email'))
             }
+            this.addAccount.add({
+                name,
+                email,
+                password
+            })
         } catch (error) {
             return serverError()
         }
