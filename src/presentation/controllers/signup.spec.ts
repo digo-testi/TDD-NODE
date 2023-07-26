5091af5811eaccf657f661b9f8551076b67a41bb
Diego Testi <diego.testi@loviz.com.vc>
2023-07-26 14:42:26 -0300

    test: ensure signupController returns 400 if passwordConfirmation fails

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -7,15 +7,6 @@ interface SutTypes {
   emailValidatorStub: EmailValidator
 }
 
-const makeEmailValidatorWithError = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid(email: string): boolean {
-      throw new Error()
-    }
-  }
-  return new EmailValidatorStub()
-}
-
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
     isValid(email: string): boolean {
@@ -77,6 +68,21 @@ describe("SignUp Controller", () => {
     expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
   });
 
+  test("Should return 400 if passwordConfirmation fails", () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email",
+        password: "any_password",
+        passwordConfirmation: 'invalid_password'
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
+  });
+
   test("Should return 400 an invalid email is provided", () => {
     const { sut, emailValidatorStub } = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
@@ -111,7 +117,7 @@ describe("SignUp Controller", () => {
   test("Should return 500 if EmailValidator throws", () => {
     const { sut, emailValidatorStub } = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
-      throw new Error
+      throw new Error()
     })
     const httpRequest = {
       body: {
@@ -125,5 +131,4 @@ describe("SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError())
   });
-
-});
+})
