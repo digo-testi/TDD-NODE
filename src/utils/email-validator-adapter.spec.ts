4e1e692f29ec556ca245051a27e03152c34fc053
Diego Testi <diego.testi@loviz.com.vc>
2023-08-04 13:48:13 -0300

    test: ensure EmailValidator calls validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -8,16 +8,23 @@ jest.mock('validator', () => ({
 }))
 
 describe("EmailValidator Adapter", () => {
-  test("should return false if validator returns false ", () => {
+  test("should return false if validator returns false", () => {
     const sut = new EmailValidatorAdapter();
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid("invalid_email@mail.com");
     expect(isValid).toBe(false);
   });
 
-  test("should return false if validator returns true ", () => {
+  test("should return false if validator returns true", () => {
     const sut = new EmailValidatorAdapter();
     const isValid = sut.isValid("validl@mail.com");
     expect(isValid).toBe(true);
+  });
+
+  test("should call validator with correct email", () => {
+    const sut = new EmailValidatorAdapter();
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid("valid@email.com");
+    expect(isEmailSpy).toHaveBeenCalledWith('valid@email.com');
   });
 });
