ee7817d6ee938991c9736691c38820de09fa0c7e
Diego Testi <diego.testi@loviz.com.vc>
2023-08-04 14:06:31 -0300

    feat: ensure DbAddAccount calls encrypter with correct password

---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,14 @@
+import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
+import { AccountModel } from '../../../domain/models/account';
+import { Encrypter } from '../../protocols/encrypter';
+
+export class DbAddAccount implements AddAccount {
+    constructor (
+        private readonly encrypter: Encrypter
+    ){}
+
+    async add (account : AddAccountModel): Promise<AccountModel> {
+        this.encrypter.encrypt(account.password)
+        return new Promise(resolve => resolve(null))
+    }
+}
