8523b48c40a18cb4339d4f3eaaee00bd62bdaa45
Diego Testi <diego.testi@loviz.com.vc>
2023-08-04 15:51:15 -0300

    feat: ensure DbAddAccount  calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,14 +1,22 @@
-import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
-import { AccountModel } from '../../../domain/models/account';
-import { Encrypter } from '../../protocols/encrypter';
+import {
+  AddAccount,
+  AddAccountModel,
+} from "../../../domain/usecases/add-account";
+import { AccountModel } from "../../../domain/models/account";
+import { Encrypter } from "../../protocols/encrypter";
+import { AddAccountRepository } from "./db-add-account-protocols";
 
 export class DbAddAccount implements AddAccount {
-    constructor (
-        private readonly encrypter: Encrypter
-    ){}
+  constructor(
+    private readonly encrypter: Encrypter,
+    private readonly addAccountRepository: AddAccountRepository
+  ) {}
 
-    async add (account : AddAccountModel): Promise<AccountModel> {
-        this.encrypter.encrypt(account.password)
-        return new Promise(resolve => resolve(null))
-    }
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password);
+    await this.addAccountRepository.add(
+      Object.assign({}, accountData, { password: hashedPassword })
+    );
+    return new Promise((resolve) => resolve(null));
+  }
 }
